1b77a7ec4a8b5f05aa58e9afd53a1a0c8a589810
David Konerding <dakoner@gmail.com>
2015-04-24 16:30:38 -0700

    add arduino_neopixel_shutter_hub

---
+++ micromanager/arduino_neopixel_shutter_hub/Arduino.cpp
@@ -0,0 +1,504 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE:          Arduino.cpp
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Arduino adapter.  Needs accompanying firmware
+// COPYRIGHT:     University of California, San Francisco, 2008
+// LICENSE:       LGPL
+// 
+// AUTHOR:        Nico Stuurman, nico@cmp.ucsf.edu 11/09/2008
+//                automatic device detection by Karl Hoover
+//
+//
+
+#include "Arduino.h"
+#include "MMDevice/ModuleInterface.h"
+#include <sstream>
+#include <cstdio>
+
+#ifdef WIN32
+   #define WIN32_LEAN_AND_MEAN
+   #include <windows.h>
+   #define snprintf _snprintf 
+#endif
+
+const char* g_DeviceNameArduinoHub = "ArduinoNeoPixel-Hub";
+const char* g_DeviceNameArduinoShutter = "ArduinoNeoPixel-Shutter";
+
+
+// static lock
+MMThreadLock CArduinoHub::lock_;
+
+///////////////////////////////////////////////////////////////////////////////
+// Exported MMDevice API
+///////////////////////////////////////////////////////////////////////////////
+MODULE_API void InitializeModuleData()
+{
+   RegisterDevice(g_DeviceNameArduinoHub, MM::HubDevice, "Hub (required)");
+   RegisterDevice(g_DeviceNameArduinoShutter, MM::ShutterDevice, "Shutter");
+}
+
+MODULE_API MM::Device* CreateDevice(const char* deviceName)
+{
+   if (deviceName == 0)
+      return 0;
+
+   if (strcmp(deviceName, g_DeviceNameArduinoHub) == 0)
+   {
+      return new CArduinoHub;
+   }
+   else if (strcmp(deviceName, g_DeviceNameArduinoShutter) == 0)
+   {
+      return new CArduinoShutter;
+   }
+
+   return 0;
+}
+
+MODULE_API void DeleteDevice(MM::Device* pDevice)
+{
+   delete pDevice;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoHUb implementation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+CArduinoHub::CArduinoHub() :
+   initialized_ (false),
+   switchState_ (0),
+   shutterState_ (0)
+{
+   portAvailable_ = false;
+   invertedLogic_ = false;
+   timedOutputActive_ = false;
+
+   InitializeDefaultErrorMessages();
+
+   SetErrorText(ERR_PORT_OPEN_FAILED, "Failed opening Arduino USB device");
+   SetErrorText(ERR_BOARD_NOT_FOUND, "Did not find an Arduino board with the correct firmware.  Is the Arduino board connected to this serial port?");
+   SetErrorText(ERR_NO_PORT_SET, "Hub Device not found.  The Arduino Hub device is needed to create this device");
+   std::ostringstream errorText;
+   errorText << "The firmware version on the Arduino is not compatible with this adapter.  Please use firmware version ";
+   // errorText <<  g_Min_MMVersion << " to " << g_Max_MMVersion;
+   SetErrorText(ERR_VERSION_MISMATCH, errorText.str().c_str());
+
+   CPropertyAction* pAct = new CPropertyAction(this, &CArduinoHub::OnPort);
+   CreateProperty(MM::g_Keyword_Port, "Undefined", MM::String, false, pAct, true);
+}
+
+CArduinoHub::~CArduinoHub()
+{
+   Shutdown();
+}
+
+void CArduinoHub::GetName(char* name) const
+{
+   CDeviceUtils::CopyLimitedString(name, g_DeviceNameArduinoHub);
+}
+
+bool CArduinoHub::Busy()
+{
+   return false;
+}
+
+// private and expects caller to:
+// 1. guard the port
+// 2. purge the port
+int CArduinoHub::GetControllerVersion(int& version)
+{
+   int ret = DEVICE_OK;
+   unsigned char command[2];
+   command[0] = '\r';
+   command[1] = '\r';
+   version = 0;
+
+  if(!portAvailable_)
+    return ERR_NO_PORT_SET;
+
+  ret = SendSerialCommand(port_.c_str(), "V", "\r");
+  if (ret != DEVICE_OK)
+      return ret;
+   LogMessage("XXXX waiting for answer:");
+
+   std::string answer;
+   ret = GetSerialAnswer(port_.c_str(), "\r", answer);
+   if (ret != DEVICE_OK) {
+     LogMessage("XXXX got error:");
+   }
+
+  ret = SendSerialCommand(port_.c_str(), "V", "\r");
+  if (ret != DEVICE_OK)
+      return ret;
+   LogMessage("XXXX waiting for answer:");
+
+   ret = GetSerialAnswer(port_.c_str(), "\r", answer);
+   if (ret != DEVICE_OK) {
+     LogMessage("XXXX got error:");
+      return ret;
+   }
+
+   LogMessage("Got:");
+     LogMessage(answer);
+     if (answer != "ArduinoNeoPixelShutter" && answer != "\nArduinoNeoPixelShutter") {
+     LogMessage("board not found:");
+      return ERR_BOARD_NOT_FOUND;
+     }
+   version = 1; 
+   return ret;
+
+}
+
+MM::DeviceDetectionStatus CArduinoHub::DetectDevice(void)
+{
+   if (initialized_)
+      return MM::CanCommunicate;
+
+   // all conditions must be satisfied...
+   MM::DeviceDetectionStatus result = MM::Misconfigured;
+   char answerTO[MM::MaxStrLength];
+   
+   try
+   {
+      std::string portLowerCase = port_;
+      for( std::string::iterator its = portLowerCase.begin(); its != portLowerCase.end(); ++its)
+      {
+         *its = (char)tolower(*its);
+      }
+      if( 0< portLowerCase.length() &&  0 != portLowerCase.compare("undefined")  && 0 != portLowerCase.compare("unknown") )
+      {
+         result = MM::CanNotCommunicate;
+         // record the default answer time out
+         GetCoreCallback()->GetDeviceProperty(port_.c_str(), "AnswerTimeout", answerTO);
+
+         // device specific default communication parameters
+         // for Arduino Duemilanova
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_Handshaking, "Off");
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_BaudRate, "9600" );
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), MM::g_Keyword_StopBits, "1");
+         // Arduino timed out in GetControllerVersion even if AnswerTimeout  = 300 ms
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "AnswerTimeout", "1000.0");
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "DelayBetweenCharsMs", "0");
+         MM::Device* pS = GetCoreCallback()->GetDevice(this, port_.c_str());
+         pS->Initialize();
+         // The first second or so after opening the serial port, the Arduino is waiting for firmwareupgrades.  Simply sleep 2 seconds.
+         CDeviceUtils::SleepMs(1000);
+         MMThreadGuard myLock(lock_);
+         PurgeComPort(port_.c_str());
+         int v = 0;
+         int ret = GetControllerVersion(v);
+         // later, Initialize will explicitly check the version #
+         if( DEVICE_OK != ret )
+         {
+            LogMessageCode(ret,true);
+         }
+         else
+         {
+            // to succeed must reach here....
+            result = MM::CanCommunicate;
+         }
+         pS->Shutdown();
+         // always restore the AnswerTimeout to the default
+         GetCoreCallback()->SetDeviceProperty(port_.c_str(), "AnswerTimeout", answerTO);
+
+      }
+   }
+   catch(...)
+   {
+      LogMessage("Exception in DetectDevice!",false);
+   }
+
+   return result;
+}
+
+
+int CArduinoHub::Initialize()
+{
+   // Name
+   int ret = CreateProperty(MM::g_Keyword_Name, g_DeviceNameArduinoHub, MM::String, true);
+   if (DEVICE_OK != ret)
+      return ret;
+
+   // The first second or so after opening the serial port, the Arduino is waiting for firmwareupgrades.  Simply sleep 1 second.
+   CDeviceUtils::SleepMs(1000);
+
+   MMThreadGuard myLock(lock_);
+
+   // Check that we have a controller:
+   PurgeComPort(port_.c_str());
+   ret = GetControllerVersion(version_);
+   if( DEVICE_OK != ret)
+      return ret;
+
+   // if (version_ < g_Min_MMVersion || version_ > g_Max_MMVersion)
+   //    return ERR_VERSION_MISMATCH;
+
+   CPropertyAction* pAct = new CPropertyAction(this, &CArduinoHub::OnVersion);
+   std::ostringstream sversion;
+   sversion << version_;
+   // CreateProperty(g_versionProp, sversion.str().c_str(), MM::Integer, true, pAct);
+
+   ret = UpdateStatus();
+   if (ret != DEVICE_OK)
+      return ret;
+
+   // turn off verbose serial debug messages
+   // GetCoreCallback()->SetDeviceProperty(port_.c_str(), "Verbose", "0");
+
+   initialized_ = true;
+   return DEVICE_OK;
+}
+
+int CArduinoHub::DetectInstalledDevices()
+{
+   if (MM::CanCommunicate == DetectDevice()) 
+   {
+      std::vector<std::string> peripherals; 
+      peripherals.clear();
+      peripherals.push_back(g_DeviceNameArduinoShutter);
+      for (size_t i=0; i < peripherals.size(); i++) 
+      {
+         MM::Device* pDev = ::CreateDevice(peripherals[i].c_str());
+         if (pDev) 
+         {
+            AddInstalledDevice(pDev);
+         }
+      }
+   }
+
+   return DEVICE_OK;
+}
+
+
+
+int CArduinoHub::Shutdown()
+{
+   initialized_ = false;
+   return DEVICE_OK;
+}
+
+int CArduinoHub::OnPort(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+      pProp->Set(port_.c_str());
+   }
+   else if (pAct == MM::AfterSet)
+   {
+      pProp->Get(port_);
+      portAvailable_ = true;
+   }
+   return DEVICE_OK;
+}
+
+int CArduinoHub::OnVersion(MM::PropertyBase* pProp, MM::ActionType pAct)
+{
+   if (pAct == MM::BeforeGet)
+   {
+      pProp->Set((long)version_);
+   }
+   return DEVICE_OK;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// CArduinoShutter implementation
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+CArduinoShutter::CArduinoShutter() : initialized_(false), name_(g_DeviceNameArduinoShutter)
+{
+   InitializeDefaultErrorMessages();
+   EnableDelay();
+
+   SetErrorText(ERR_NO_PORT_SET, "Hub Device not found.  The Arduino Hub device is needed to create this device");
+
+   // Name
+   int ret = CreateProperty(MM::g_Keyword_Name, g_DeviceNameArduinoShutter, MM::String, true);
+   assert(DEVICE_OK == ret);
+
+   // Description
+   ret = CreateProperty(MM::g_Keyword_Description, "Arduino shutter driver", MM::String, true);
+   assert(DEVICE_OK == ret);
+
+   // parent ID display
+   CreateHubIDProperty();
+}
+
+CArduinoShutter::~CArduinoShutter()
+{
+   Shutdown();
+}
+
+void CArduinoShutter::GetName(char* name) const
+{
+   CDeviceUtils::CopyLimitedString(name, g_DeviceNameArduinoShutter);
+}
+
+bool CArduinoShutter::Busy()
+{
+   MM::MMTime interval = GetCurrentMMTime() - changedTime_;
+
+   if (interval < (1000.0 * GetDelayMs() ))
+      return true;
+   else
+       return false;
+}
+
+int CArduinoShutter::Initialize()
+{
+   CArduinoHub* hub = static_cast<CArduinoHub*>(GetParentHub());
+   if (!hub || !hub->IsPortAvailable()) {
+      return ERR_NO_PORT_SET;
+   }
+   char hubLabel[MM::MaxStrLength];
+   hub->GetLabel(hubLabel);
+   SetParentID(hubLabel); // for backward comp.
+
+   // set property list
+   // -----------------
+   
+   // OnOff
+   // ------
+   CPropertyAction* pAct = new CPropertyAction (this, &CArduinoShutter::OnOnOff);
+   int ret = CreateProperty("OnOff", "0", MM::Integer, false, pAct);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   // set shutter into the off state
+   //WriteToPort(0);
+   
+   std::vector<std::string> vals;
+   vals.push_back("0");
+   vals.push_back("1");
+   ret = SetAllowedValues("OnOff", vals);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   ret = UpdateStatus();
+   if (ret != DEVICE_OK)
+      return ret;
+
+   changedTime_ = GetCurrentMMTime();
+   initialized_ = true;
+
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::Shutdown()
+{
+   if (initialized_)
+   {
+      initialized_ = false;
+   }
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::SetOpen(bool open)
+{
+	std::ostringstream os;
+	os << "Request " << open;
+	LogMessage(os.str().c_str(), true);
+
+   if (open)
+      return SetProperty("OnOff", "1");
+   else
+      return SetProperty("OnOff", "0");
+}
+
+int CArduinoShutter::GetOpen(bool& open)
+{
+   char buf[MM::MaxStrLength];
+   int ret = GetProperty("OnOff", buf);
+   if (ret != DEVICE_OK)
+      return ret;
+   long pos = atol(buf);
+   pos > 0 ? open = true : open = false;
+
+   return DEVICE_OK;
+}
+
+int CArduinoShutter::Fire(double /*deltaT*/)
+{
+   return DEVICE_UNSUPPORTED_COMMAND;
+}
+
+int CArduinoShutter::WriteToPort(long value)
+{
+   CArduinoHub* hub = static_cast<CArduinoHub*>(GetParentHub());
+   if (!hub || !hub->IsPortAvailable())
+      return ERR_NO_PORT_SET;
+
+   MMThreadGuard myLock(hub->GetLock());
+
+   value = 63 & value;
+   if (hub->IsLogicInverted())
+      value = ~value;
+
+   hub->PurgeComPortH();
+
+   unsigned char command[2];
+   command[0] = 1;
+   command[1] = (unsigned char) value;
+   int ret = hub->WriteToComPortH((const unsigned char*) command, 2);
+   if (ret != DEVICE_OK)
+      return ret;
+
+   MM::MMTime startTime = GetCurrentMMTime();
+   unsigned long bytesRead = 0;
+   unsigned char answer[1];
+   while ((bytesRead < 1) && ( (GetCurrentMMTime() - startTime).getMsec() < 250)) {
+      ret = hub->ReadFromComPortH(answer, 1, bytesRead);
+      if (ret != DEVICE_OK)
+         return ret;
+   }
+   if (answer[0] != 1)
+      return ERR_COMMUNICATION;
+
+   hub->SetTimedOutput(false);
+
+   return DEVICE_OK;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Action handlers
+///////////////////////////////////////////////////////////////////////////////
+
+int CArduinoShutter::OnOnOff(MM::PropertyBase* pProp, MM::ActionType eAct)
+{
+  LogMessage("YYYY");
+   CArduinoHub* hub = static_cast<CArduinoHub*>(GetParentHub());
+   if (eAct == MM::BeforeGet)
+   {
+  LogMessage("YYYY1");
+      // use cached state
+      pProp->Set((long)hub->GetShutterState());
+   }
+   else if (eAct == MM::AfterSet)
+   {
+  LogMessage("YYYY2");
+      long pos;
+      pProp->Get(pos);
+      int ret;
+  LogMessage("YYYY3");
+      if (pos == 0) {
+	std::string command = "P0\r";
+	int ret = hub->WriteToComPortH((const unsigned char*) command.c_str(), command.length());
+	if (ret != DEVICE_OK)
+	  return ret;
+      }
+      else {
+	std::string command = "P65535\r";
+	int ret = hub->WriteToComPortH((const unsigned char*) command.c_str(), command.length());
+	if (ret != DEVICE_OK)
+	  return ret;
+      }
+      hub->SetShutterState(pos);
+      changedTime_ = GetCurrentMMTime();
+   }
+
+   return DEVICE_OK;
+}
+
+
+
