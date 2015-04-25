e90fcd23fc5217f5453868849ca5a781f815c2ec
David Konerding <dakoner@gmail.com>
2015-04-25 09:35:18 -0700

    cleanup.

--- micromanager/arduino_neopixel_shutter_hub/Arduino.h
+++ micromanager/arduino_neopixel_shutter_hub/Arduino.h
@@ -52,14 +52,10 @@ public:
 
    // property handlers
    int OnPort(MM::PropertyBase* pPropt, MM::ActionType eAct);
-   int OnLogic(MM::PropertyBase* pPropt, MM::ActionType eAct);
    int OnVersion(MM::PropertyBase* pPropt, MM::ActionType eAct);
 
    // custom interface for child devices
    bool IsPortAvailable() {return portAvailable_;}
-   bool IsLogicInverted() {return invertedLogic_;}
-   bool IsTimedOutputActive() {return timedOutputActive_;}
-   void SetTimedOutput(bool active) {timedOutputActive_ = active;}
 
    int PurgeComPortH() {return PurgeComPort(port_.c_str());}
    int WriteToComPortH(const unsigned char* command, unsigned len) {return WriteToComPort(port_.c_str(), command, len);}
@@ -78,8 +74,6 @@ private:
    std::string port_;
    bool initialized_;
    bool portAvailable_;
-   bool invertedLogic_;
-   bool timedOutputActive_;
    int version_;
    static MMThreadLock lock_;
    unsigned switchState_;
@@ -115,157 +109,5 @@ private:
    bool initialized_;
    std::string name_;
 };
-
-class CArduinoSwitch : public CStateDeviceBase<CArduinoSwitch>  
-{
-public:
-   CArduinoSwitch();
-   ~CArduinoSwitch();
-  
-   // MMDevice API
-   // ------------
-   int Initialize();
-   int Shutdown();
-  
-   void GetName(char* pszName) const;
-   bool Busy() {return busy_;}
-   
-   unsigned long GetNumberOfPositions()const {return numPos_;}
-
-   // action interface
-   // ----------------
-   int OnState(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnDelay(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnRepeatTimedPattern(MM::PropertyBase* pProp, MM::ActionType eAct);
-   /*
-   int OnSetPattern(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnGetPattern(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnPatternsUsed(MM::PropertyBase* pProp, MM::ActionType eAct);
-   */
-   int OnSkipTriggers(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnStartTrigger(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnStartTimedOutput(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnBlanking(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnBlankingTriggerDirection(MM::PropertyBase* pProp, MM::ActionType eAct);
-
-   int OnSequence(MM::PropertyBase* pProp, MM::ActionType eAct);
-
-private:
-   static const unsigned int NUMPATTERNS = 12;
-
-   int OpenPort(const char* pszName, long lnValue);
-   int WriteToPort(long lnValue);
-   int ClosePort();
-   int LoadSequence(unsigned size, unsigned char* seq);
-
-   unsigned pattern_[NUMPATTERNS];
-   unsigned delay_[NUMPATTERNS];
-   int nrPatternsUsed_;
-   unsigned currentDelay_;
-   bool sequenceOn_;
-   bool blanking_;
-   bool initialized_;
-   long numPos_;
-   bool busy_;
-};
-
-class CArduinoDA : public CSignalIOBase<CArduinoDA>  
-{
-public:
-   CArduinoDA(int channel);
-   ~CArduinoDA();
-  
-   // MMDevice API
-   // ------------
-   int Initialize();
-   int Shutdown();
-  
-   void GetName(char* pszName) const;
-   bool Busy() {return busy_;}
-
-   // DA API
-   int SetGateOpen(bool open);
-   int GetGateOpen(bool& open) {open = gateOpen_; return DEVICE_OK;};
-   int SetSignal(double volts);
-   int GetSignal(double& volts) {volts_ = volts; return DEVICE_UNSUPPORTED_COMMAND;}     
-   int GetLimits(double& minVolts, double& maxVolts) {minVolts = minV_; maxVolts = maxV_; return DEVICE_OK;}
-   
-   int IsDASequenceable(bool& isSequenceable) const {isSequenceable = false; return DEVICE_OK;}
-
-   // action interface
-   // ----------------
-   int OnVolts(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnMaxVolt(MM::PropertyBase* pProp, MM::ActionType eAct);
-   int OnChannel(MM::PropertyBase* pProp, MM::ActionType eAct);
-
-private:
-   int WriteToPort(unsigned long lnValue);
-   int WriteSignal(double volts);
-
-   bool initialized_;
-   bool busy_;
-   double minV_;
-   double maxV_;
-   double volts_;
-   double gatedVolts_;
-   unsigned channel_;
-   unsigned maxChannel_;
-   bool gateOpen_;
-   std::string name_;
-};
-
-class CArduinoInput : public CGenericBase<CArduinoInput>  
-{
-public:
-   CArduinoInput();
-   ~CArduinoInput();
-
-   int Initialize();
-   int Shutdown();
-   void GetName(char* pszName) const;
-   bool Busy();
-
-   int OnDigitalInput(MM::PropertyBase* pPropt, MM::ActionType eAct);
-   int OnAnalogInput(MM::PropertyBase* pProp, MM::ActionType eAct, long channel);
-
-   int GetDigitalInput(long* state);
-   int ReportStateChange(long newState);
-
-private:
-   int ReadNBytes(CArduinoHub* h, unsigned int n, unsigned char* answer);
-   int SetPullUp(int pin, int state);
-
-   MMThreadLock lock_;
-   ArduinoInputMonitorThread* mThread_;
-   char pins_[MM::MaxStrLength];
-   char pullUp_[MM::MaxStrLength];
-   int pin_;
-   bool initialized_;
-   std::string name_;
-};
-
-class ArduinoInputMonitorThread : public MMDeviceThreadBase
-{
-   public:
-      ArduinoInputMonitorThread(CArduinoInput& aInput);
-     ~ArduinoInputMonitorThread();
-      int svc();
-      int open (void*) { return 0;}
-      int close(unsigned long) {return 0;}
-
-      void Start();
-      void Stop() {stop_ = true;}
-      ArduinoInputMonitorThread & operator=( const ArduinoInputMonitorThread & ) 
-      {
-         return *this;
-      }
-
-
-   private:
-      long state_;
-      CArduinoInput& aInput_;
-      bool stop_;
-};
-
 
 #endif //_Arduino_H_
