cbd3d13b542b5b1c80e4a5f40817bb95c1e7d346
David Konerding <dakoner@gmail.com>
2015-05-05 12:50:14 -0700

    update qt example to use optical flow.

--- mm_to_opencv/mm_qt.py
+++ mm_to_opencv/mm_qt.py
@@ -31,7 +31,6 @@ class MM(QtCore.QThread):
         return self.img
     
     def stop(self):
-        # cv2.destroyAllWindows()
         self.mmc.stopSequenceAcquisition()
         self.mmc.reset()
 
@@ -42,29 +41,63 @@ class MainWindow(QtGui.QMainWindow):
 
 
         self.mm = MM()
+        self.prev = self.mm.getImage()
         self.mm.start()
 
-        image = cv2.cvtColor(self.mm.getImage(), cv2.COLOR_GRAY2RGB)
-        self.image = QtGui.QImage(image.data, 640, 480, QtGui.QImage.Format_RGB888)
-        # self.image.fill(QtGui.qRgb(255,0,0))
+        prevrgb = cv2.cvtColor(self.prev, cv2.COLOR_GRAY2RGB)
+        self.image = QtGui.QImage(prevrgb.data, 640, 480, QtGui.QImage.Format_RGB888)
         self.pix = QtGui.QPixmap.fromImage(self.image)
 
         self.lbl = QtGui.QLabel(self)
         self.lbl.setPixmap(self.pix)
         self.lbl.show()
-        self.setCentralWidget(self.lbl)
+
+        self.widget = QtGui.QWidget(self)
+        self.hbox = QtGui.QHBoxLayout()
+        self.widget.setLayout(self.hbox)    
+        self.hbox.addStretch(1)
+        self.hbox.addWidget(self.lbl)
+        self.setCentralWidget(self.widget)
+       
 
         self.timer = QtCore.QTimer()
         self.timer.start(.04)
         self.connect(self.timer, QtCore.SIGNAL('timeout()'), self.updateData) 
 
+        # self.fgbg = cv2.BackgroundSubtractorMOG(500, 6, 0.9, 1)
+        self.fgbg = cv2.BackgroundSubtractorMOG2()
+        
 
     def updateData(self):
-        image = cv2.cvtColor(self.mm.getImage(), cv2.COLOR_GRAY2RGB)
+        img = self.mm.getImage()
+
+        flow = cv2.calcOpticalFlowFarneback(self.prev, img, 0.5, 3, 15, 3, 5, 1.2, 0)
+        w, h = 640, 480
+        step = 16
+        y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
+        fx, fy = flow[y,x].T
+        polys = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+        polys = np.int32(polys + 0.5)
+
+        self.prev = img
+
+
+        # fgmask = self.fgbg.apply(img)
+        # draw = img & fgmask
+        image = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
         self.image = QtGui.QImage(image.data, 640, 480, QtGui.QImage.Format_RGB888)
+        painter = QtGui.QPainter(self.image)
+        blue = QtGui.QColor()
+        blue.setNamedColor("blue")
+        painter.setPen(QtGui.QPen(blue))
+        for lines in polys:
+            p1 = QtCore.QPoint(*lines[0])
+            p2 = QtCore.QPoint(*lines[1])
+            l = QtCore.QLine(p1, p2)
+            painter.drawLine(l)
+
         self.pix.convertFromImage(self.image)
         self.repaint()
-        # self.lbl.setPixmap(self.pix)
     
                         
 if __name__ == '__main__':
