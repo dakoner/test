2c9d95c7e7935fc5e58e406ed114bb40911b0358
David Konerding <dakoner@gmail.com>
2015-05-13 18:17:52 -0700

    many updates.

--- mm_to_opencv/qt_standalone.py
+++ mm_to_opencv/qt_standalone.py
@@ -44,8 +44,8 @@ class MainWindow(QtGui.QMainWindow):
         self.timer.start(.01)
         self.connect(self.timer, QtCore.SIGNAL('timeout()'), self.updateData) 
 
-        # self.fgbg = cv2.BackgroundSubtractorMOG(500, 6, 0.9, 1)
-        self.fgbg = cv2.BackgroundSubtractorMOG2()
+        self.fgbg = cv2.BackgroundSubtractorMOG(500, 6, 0.9, 1)
+        # self.fgbg = cv2.BackgroundSubtractorMOG2()
 
         self.frame = 0
         self.resize(self.width, self.height*2)
@@ -60,30 +60,30 @@ class MainWindow(QtGui.QMainWindow):
         if ret:
             self.image = QtGui.QImage(img.data, self.width, self.height, QtGui.QImage.Format_RGB888).rgbSwapped()
 
-            p = cv2.cvtColor(self.prev, cv2.COLOR_BGR2GRAY)
+            # p = cv2.cvtColor(self.prev, cv2.COLOR_BGR2GRAY)
             i = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(p, i, 0.5, 3, 15, 3, 5, 1.2, 0)
-            w, h = self.width, self.height
-            step = 16
-            y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
-            fx, fy = flow[y,x].T
-            polys = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
-            polys = np.int32(polys + 0.5)
-            painter = QtGui.QPainter(self.image)
-            blue = QtGui.QColor()
-            blue.setNamedColor("blue")
-            painter.setPen(QtGui.QPen(blue))
-            for lines in polys:
-                p1 = QtCore.QPoint(*lines[0])
-                p2 = QtCore.QPoint(*lines[1])
-                l = QtCore.QLine(p1, p2)
-                painter.drawLine(l)
+            # flow = cv2.calcOpticalFlowFarneback(p, i, 0.5, 3, 15, 3, 5, 1.2, 0)
+            # w, h = self.width, self.height
+            # step = 16
+            # y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1)
+            # fx, fy = flow[y,x].T
+            # polys = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+            # polys = np.int32(polys + 0.5)
+            # painter = QtGui.QPainter(self.image)
+            # blue = QtGui.QColor()
+            # blue.setNamedColor("blue")
+            # painter.setPen(QtGui.QPen(blue))
+            # for lines in polys:
+            #     p1 = QtCore.QPoint(*lines[0])
+            #     p2 = QtCore.QPoint(*lines[1])
+            #     l = QtCore.QLine(p1, p2)
+            #     painter.drawLine(l)
 
             fgmask = self.fgbg.apply(i)
             fgmask  = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-            draw = img & fgmask
+            # draw = img & fgmask
             self.prev = img
-            self.image2 = QtGui.QImage(draw.data, self.width, self.height, QtGui.QImage.Format_RGB888).rgbSwapped()
+            self.image2 = QtGui.QImage(fgmask.data, self.width, self.height, QtGui.QImage.Format_RGB888).rgbSwapped()
 
 
 
