3ac73f5e9a535e16e2037640b8e83bc34c628ae2
David Konerding <dek@google.com>
2015-05-19 15:11:38 -0700

    minor build.sh update.

--- light/build.sh
+++ light/build.sh
@@ -4,7 +4,8 @@ set -eu
 ARDUINO_ROOT=/usr/local/google/home/dek/arduino-1.5.8
 ARDUINO_TOOLS=$ARDUINO_ROOT/hardware/tools/avr/bin
 
-$ARDUINO_TOOLS/avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=158 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR -I$ARDUINO_ROOT/hardware/arduino/avr/cores/arduino -I$ARDUINO_ROOT/hardware/arduino/avr/variants/standard light.ino -o light.cpp.o 
+ln -s light.ino light.cpp
+$ARDUINO_TOOLS/avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=158 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR -I$ARDUINO_ROOT/hardware/arduino/avr/cores/arduino -I$ARDUINO_ROOT/hardware/arduino/avr/variants/standard light.cpp -o light.cpp.o 
 $ARDUINO_TOOLS/avr-g++ -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=158 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR -I$ARDUINO_ROOT/hardware/arduino/avr/cores/arduino -I$ARDUINO_ROOT/hardware/arduino/avr/variants/standard -I. Adafruit_NeoPixel.cpp -o Adafruit_NeoPixel.cpp.o 
 $ARDUINO_TOOLS/avr-gcc -c -g -Os -w -ffunction-sections -fdata-sections -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=158 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR -I$ARDUINO_ROOT/hardware/arduino/avr/cores/arduino -I$ARDUINO_ROOT/hardware/arduino/avr/variants/standard $ARDUINO_ROOT/hardware/arduino/avr/cores/arduino/wiring_pulse.c -o wiring_pulse.c.o 
 $ARDUINO_TOOLS/avr-gcc -c -g -Os -w -ffunction-sections -fdata-sections -MMD -mmcu=atmega328p -DF_CPU=16000000L -DARDUINO=158 -DARDUINO_AVR_UNO -DARDUINO_ARCH_AVR -I$ARDUINO_ROOT/hardware/arduino/avr/cores/arduino -I$ARDUINO_ROOT/hardware/arduino/avr/variants/standard $ARDUINO_ROOT/hardware/arduino/avr/cores/arduino/wiring.c -o wiring.c.o 
@@ -58,4 +59,5 @@ $ARDUINO_TOOLS/avr-gcc -w -Os -Wl,--gc-sections -mmcu=atmega328p -o light.cpp.el
 $ARDUINO_TOOLS/avr-objcopy -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 light.cpp.elf light.cpp.eep 
 $ARDUINO_TOOLS/avr-objcopy -O ihex -R .eeprom light.cpp.elf light.cpp.hex 
 
-$ARDUINO_TOOLS/avrdude -C$ARDUINO_ROOT/hardware/tools/avr/etc/avrdude.conf -v -v -v -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:light.cpp.hex:i  || rm -f *.d *.o light.cpp.elf light.cpp.eep core.a light.cpp.hex
+$ARDUINO_TOOLS/avrdude -C$ARDUINO_ROOT/hardware/tools/avr/etc/avrdude.conf -v -v -v -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:light.cpp.hex:i
+rm -f *.d *.o light.cpp.elf light.cpp.eep core.a light.cpp.hex
